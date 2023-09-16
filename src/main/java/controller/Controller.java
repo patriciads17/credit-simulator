d65191285c287272b3447a0571609d9a8ec4fb19
patriciads17 <patriciadevitas30@gmail.com>
2023-09-17 00:24:47 +0700

    add controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -4,10 +4,47 @@
  */
 package controller;
 
+import java.util.Calendar;
+import java.util.List;
+import model.Cicilan;
+import model.Pinjaman;
+import view.Input;
+import view.Output;
+
 /**
  *
  * @author LENOVO
  */
 public class Controller {
+    private Pinjaman pinjaman;
     
+    public void createPinjaman() {
+        pinjaman = Input.readInput();
+        if (pinjaman.getKondisiKendaraan().equals("Baru") && pinjaman.getTahunKendaraan() < Calendar.getInstance().get(Calendar.YEAR) - 1) {
+            System.out.println("Tahun kendaraan harus lebih besar atau sama dengan tahun sekarang.");
+            return;
+        }
+
+        if (pinjaman.getTenorPinjaman() > 6) {
+            System.out.println("Tenor pinjaman tidak boleh lebih dari 6 tahun.");
+            return;
+        }
+
+        if (pinjaman.getJenisKendaraan().equals("Mobil")) {
+            if (pinjaman.getJumlahDP() < (pinjaman.getJumlahPinjaman() * 0.35)) {
+                System.out.println("Jumlah DP untuk mobil baru harus lebih besar atau sama dengan 35% dari jumlah pinjaman.");
+                return;
+            }
+        } else {
+            if (pinjaman.getJumlahDP() < (pinjaman.getJumlahPinjaman() * 0.25)) {
+                System.out.println("Jumlah DP untuk motor baru harus lebih besar atau sama dengan 25% dari jumlah pinjaman.");
+            }
+        }
+        showCicilan(pinjaman);
+    }
+    
+    public void showCicilan(Pinjaman pinjaman){
+        List<Cicilan> cicilan = pinjaman.getCicilan();
+        Output.printOutput(cicilan);
+    }
 }
